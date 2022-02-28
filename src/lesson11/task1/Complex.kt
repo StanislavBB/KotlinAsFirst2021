a4ba91895e2854ccd4ac8a2233539e7a0050483f
StanislavBB <stanislav-balan03@mail.ru>
2022-03-01 00:52:16 +0300

    Less 11. Complex

--- src/lesson11/task1/Complex.kt
+++ src/lesson11/task1/Complex.kt
@@ -5,7 +5,22 @@ package lesson11.task1
 /**
  * Фабричный метод для создания комплексного числа из строки вида x+yi
  */
-fun Complex(s: String): Complex = TODO()
+fun Complex(s: String): Complex {
+    val isNegative = "-" in s
+    val delimeter: String
+    if (isNegative) {
+        delimeter = "-"
+    } else {
+        delimeter = "+"
+    }
+    val parts = s.split(delimeter, "i")
+    val re = parts[0].toDouble()
+    var im = parts[1].toDouble()
+    if (isNegative) {
+        im = -im
+    }
+    return Complex(re, im)
+}
 
 /**
  * Класс "комплексное число".
@@ -21,40 +36,69 @@ class Complex(val re: Double, val im: Double) {
     /**
      * Конструктор из вещественного числа
      */
-    constructor(x: Double) : this(TODO(), TODO())
+    constructor(x: Double) : this(x, 0.0)
 
     /**
      * Сложение.
      */
-    operator fun plus(other: Complex): Complex = TODO()
+    operator fun plus(other: Complex): Complex = Complex(this.re + other.re, this.im + other.im)
 
     /**
      * Смена знака (у обеих частей числа)
      */
-    operator fun unaryMinus(): Complex = TODO()
+    operator fun unaryMinus(): Complex = Complex(-this.re, -this.im)
 
     /**
      * Вычитание
      */
-    operator fun minus(other: Complex): Complex = TODO()
+    operator fun minus(other: Complex): Complex = Complex(this.re - other.re, this.im - other.im)
 
     /**
      * Умножение
      */
-    operator fun times(other: Complex): Complex = TODO()
+    operator fun times(other: Complex): Complex {
+        val a = this.re
+        val b = this.im
+        val c = other.re
+        val d = other.im
+        val re = (a * c - b * d)
+        val im = (b * c + a * d)
+        return Complex(re, im)
+    }
 
     /**
      * Деление
      */
-    operator fun div(other: Complex): Complex = TODO()
+    operator fun div(other: Complex): Complex {
+        val a = this.re
+        val b = this.im
+        val c = other.re
+        val d = other.im
+        val re = (a * c + b * d) / (c * c + d * d)
+        val im = (b * c - a * d) / (c * c + d * d)
+        return Complex(re, im)
+    }
 
     /**
      * Сравнение на равенство
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean {
+        if (other !is Complex) {
+            return false
+        }
+        if (other == null) {
+            return false
+        }
+        return this.re == other.re && this.im == other.im
+    }
 
     /**
      * Преобразование в строку
      */
-    override fun toString(): String = TODO()
+    override fun toString(): String = "$re+${im}i"
+    override fun hashCode(): Int {
+        var result = re.hashCode()
+        result = 31 * result + im.hashCode()
+        return result
+    }
 }
